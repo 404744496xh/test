c27406a1e1ac3de0ffc07f3e0a6aeda4af0d0959
XH <404744496@qq.com>
2023-09-29 18:26:16 +0800

    co1 commit

---
+++ src/GitTest.java
@@ -0,0 +1,9 @@
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+
+    }
+}
